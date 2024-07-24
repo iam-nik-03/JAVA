456e5a2e249db1fbbc1dcdb6bff9d4a2fd6f1139
Nik <nikdc03x@gmail.com>
2024-07-25 00:32:15 +0530

    first commit

---
+++ Pattern/Alphabet/Alphabet.java
@@ -0,0 +1,14 @@
+public class Alphabet{
+    public static void main(String args[]){
+        int n = 4;
+        char ch = 'A';
+        for (int line = 1; line <= n; line++) {
+            for (int alpha = 1; alpha <= line; alpha++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    
+    }
+}
---
+++ Pattern/star/starpattern.java
@@ -0,0 +1,31 @@
+//star pattern  star 
+class inStar{
+ public static void main (String args[]){
+    int n=4;
+
+   for ( int line=1; line<=n; line++){
+    for( int star=1; star<=line;star++){
+        System.out.print("*");
+    }
+ System.out.println();
+
+   }
+   }
+}
+
+// //invertes star 
+// class inStar{
+//  public static void main (String args[]){
+//     int n=4;
+
+//    for ( int line=1; line<=n; line++){
+//     for( int star=1; star<=n-line+1;star++){
+//         System.out.print(" *");
+//     }
+//  System.out.println();
+
+//    }
+//    }
+// }
+
+
