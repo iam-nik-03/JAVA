b645792d9698f22dc4010000aff5e14f7ca94c47
Nik <nikdc03x@gmail.com>
2024-07-27 23:01:01 +0530

    binoCoeff

--- Function/facto.java
+++ Function/facto.java
@@ -1,4 +1,22 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class facto {
+//     public static int factorial(int n) {
+//         int f = 1;
+//         for (int i = 1; i <= n; i++) {
+//             f = f * i;
+//         }
+//         return f;
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.print("Enter the  Values : ");
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//        System.out.println(n +"! = "+ factorial(n));
+        
+//     }
+// }
 
 public class facto {
     public static int factorial(int n) {
@@ -9,11 +27,18 @@ public class facto {
         return f;
     }
 
-    public static void main(String[] args) {
-        System.out.print("Enter the  Values : ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-       System.out.println(n +"! = "+ factorial(n));
+    public static int BinoCoeff(int n , int r){
+        int fact_n= factorial(n);
+        int fact_r= factorial(r);
+        int fact_nmr= factorial(n-r);
         
+        int BinoCoeff = fact_n / (fact_r * fact_nmr);
+        return BinoCoeff;
+    
+        
+    }
+    public static void main(String[] args) {
+      
+System.out.println(BinoCoeff(5,2));        
     }
 }
